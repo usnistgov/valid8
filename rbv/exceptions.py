4a68d5c7b02408b5776176eb1c4a1243c43e7151
Marion <marion.le-bras@esial.net>
2019-01-16 23:13:24 -0500

    better displaying of exceptions

--- rbv/exceptions.py
+++ rbv/exceptions.py
@@ -1,11 +1,17 @@
 class BaseValidationError(Exception):
-    pass
+    def __init__(self, message, *args):
+        self.message = message
+        super().__init__(message, *args)
+
+    def __str__(self):
+        return self.message
 
 
 class NoSuchFile(BaseValidationError):
-    def __init__(self, target):
-        self.target = target
-        self.message = f"{target} was not found."
+    def __init__(self, filepath):
+        self.target = filepath
+        self.message = f"'{filepath}' was not found."
+        super().__init__(self.message)
 
 
 class IncorrectFileCount(BaseValidationError):
@@ -13,7 +19,24 @@ class IncorrectFileCount(BaseValidationError):
         self.requested_count = requested_count
         self.files_found = files_found
         self.message = f"{len(files_found)} files found, {requested_count} requested."
+        super().__init__(self.message)
 
 
 class UnknownRule(BaseValidationError):
     """"""
+
+    def __init__(self, rule):
+        self.rule = rule
+        self.message = f"'{rule}' was not recognized."
+        super().__init__(self.message)
+
+
+class PatternNotFound(BaseValidationError):
+    def __init__(self, pattern, before_subs=None):
+        self.target = pattern
+        self.message = f"Pattern '{pattern}' not found."
+        self.before_subs = before_subs
+        super().__init__(self.message, self.before_subs)
+
+
+# TODO could dataclasses be useful here?
