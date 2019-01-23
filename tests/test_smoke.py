661d2a07bce2711fa033f359e450151f5286f670
Marion <marion.le-bras@esial.net>
2019-01-23 14:11:51 -0500

    Tests: refactoring and adding support for todo feature
    ```
    --directory other-dir
    ```

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -14,7 +14,7 @@ dse_rules_file = "examples/dse_rules.yml"
 single_rule_filter_action = "examples/single_rule_single_action.yml"
 single_rule_filter_action_FALSE = "examples/FALSE_single_rule_single_action.yml"
 
-YAMLConfig = namedtuple("YAMLConfig", "filepath, cdir, success")
+YAMLConfig = namedtuple("YAMLConfig", "filepath, cdir, expected")
 config_files_data = [
     YAMLConfig(dse_rules_file, ".", False),
     YAMLConfig(single_rule_filter_action, ".", True),
@@ -23,20 +23,48 @@ config_files_data = [
 ]
 
 
-@pytest.fixture(params=config_files_data)
-def config_conditions(request):
+@pytest.mark.smoke
+@pytest.mark.parametrize(
+    "full_path, current_dir, expected",
+    config_files_data,
+    indirect=["full_path", "current_dir"],
+)
+def test_expected_output(full_path, current_dir, expected, capsys):
+    test_args = ["test", "validate", full_path]
+    compare_main_with_expected_output(test_args, expected, capsys)
+
+
+@pytest.mark.smoke
+@pytest.mark.skip(reason="Requires impleming --directory feature")
+@pytest.mark.parametrize("filepath, targeted_dir, expected", config_files_data)
+def test_expected_output_when_specifying_dir(filepath, targeted_dir, expected, capsys):
+    test_args = ["test", "validate", "--directory", targeted_dir, filepath]
+    compare_main_with_expected_output(test_args, expected, capsys)
+
+
+def compare_main_with_expected_output(test_args, expected, capsys):
+    with patch.object(sys, "argv", test_args):
+        try:
+            cli.main()
+        except SystemExit as sysexit:
+            out, err = capsys.readouterr()
+            if expected is True:
+                assert sysexit.code == 0
+                assert out.strip().endswith("True")
+            else:
+                assert sysexit != 0
+                assert out.strip().endswith("False")
+
+
+@pytest.fixture
+def current_dir(request):
     # save current directory
     original_dir = Path.cwd()
-    original_filepath = Path(request.param.filepath)
-    requested_dir = Path(request.param.cdir)
+    requested_dir = Path(request.param)
 
     os.chdir(requested_dir.as_posix())
 
-    yield YAMLConfig(
-        filepath=(TEST_EXEC_DIR / original_filepath).as_posix(),
-        cdir=requested_dir,
-        success=request.param.success,
-    )
+    yield
 
     # revert to original directory at teardown
     os.chdir(original_dir.as_posix())
@@ -48,85 +76,55 @@ def full_path(tmp_path, request):
     return (TEST_EXEC_DIR / original_filepath).as_posix()
 
 
-@pytest.mark.smoke
-def test_expected_output(config_conditions, capsys):
-    test_args = ["test", "validate", config_conditions.filepath]
-    with patch.object(sys, "argv", test_args):
-        try:
-            cli.main()
-        except SystemExit as sysexit:
-            out, err = capsys.readouterr()
-            if config_conditions.success is True:
-                assert sysexit.code == 0
-                assert out.strip().endswith("True")
-            else:
-                assert sysexit != 0
-                assert out.strip().endswith("False")
-
-            assert not out.strip().endswith("sanitycheck")
-
-
-ScenarioConfig = namedtuple("ScenarioConfig", "filepath, files, success")
+ScenarioConfig = namedtuple("ScenarioConfig", "filepath, files, expected")
 scenarios_data = [
     ScenarioConfig(
-        filepath="examples/dse_rules.yml", files=["predictions.csv"], success=True
+        filepath="examples/dse_rules.yml", files=["predictions.csv"], expected=True
     ),
     ScenarioConfig(
         filepath="examples/dse_rules.yml",
         files=["predictions.csv", "other.sh"],
-        success=True,
+        expected=True,
     ),
     ScenarioConfig(
         filepath="examples/dse_rules.yml",
         files=["subdir/predictions.csv", "other.sh"],
-        success=False,
+        expected=False,
     ),
     ScenarioConfig(
-        filepath="examples/dse_rules.yml", files=["other.sh"], success=False
+        filepath="examples/dse_rules.yml", files=["other.sh"], expected=False
     ),
 ]
 
 
-@pytest.fixture(params=scenarios_data)
-def scenario_config_conditions(request, tmp_path):
+@pytest.fixture
+def dir_structure(request, tmp_path):
     # save current directory
     original_dir = Path.cwd()
-    original_filepath = Path(request.param.filepath)
     requested_dir = tmp_path / "testdir"
     requested_dir.mkdir()
-    for f in request.param.files:
+    for f in request.param:
         f_path = requested_dir / Path(f)
         f_path.parent.mkdir(parents=True, exist_ok=True)
         f_path.touch()
 
     os.chdir(requested_dir.as_posix())
 
-    yield ScenarioConfig(
-        filepath=(TEST_EXEC_DIR / original_filepath).as_posix(),
-        files=request.param.files,
-        success=request.param.success,
-    )
+    yield
 
     # revert to original directory at teardown
     os.chdir(original_dir.as_posix())
 
 
 @pytest.mark.smoke
-def test_with_fake_structure(scenario_config_conditions, capsys):
-    test_args = ["test", "validate", scenario_config_conditions.filepath]
-    with patch.object(sys, "argv", test_args):
-        try:
-            cli.main()
-        except SystemExit as sysexit:
-            out, err = capsys.readouterr()
-            if scenario_config_conditions.success is True:
-                assert sysexit.code == 0
-                assert out.strip().endswith("True")
-            else:
-                assert sysexit != 0
-                assert out.strip().endswith("False")
-
-            assert not out.strip().endswith("sanitycheck")
+@pytest.mark.parametrize(
+    "full_path, dir_structure, expected",
+    scenarios_data,
+    indirect=["full_path", "dir_structure"],
+)
+def test_with_fake_structure(full_path, dir_structure, expected, capsys):
+    test_args = ["test", "validate", full_path]
+    compare_main_with_expected_output(test_args, expected, capsys)
 
 
 ds_single_pipeline = [
@@ -153,28 +151,9 @@ ds_sp_extra_exec = ds_single_pipeline + ["executables/pipelineID"]
 @pytest.mark.parametrize("full_path", ["examples/d3m_ta1.yml"], indirect=True)
 def test_d3m_ta1(dirstructure, full_path, expected, capsys):
     test_args = ["test", "validate", full_path]
-    with patch.object(sys, "argv", test_args):
-        try:
-            cli.main()
-        except SystemExit as sysexit:
-            out, err = capsys.readouterr()
-            if expected is True:
-                assert sysexit.code == 0
-                assert out.strip().endswith("True")
-            else:
-                assert sysexit != 0
-                assert out.strip().endswith("False")
-
-            assert not out.strip().endswith("sanitycheck")
+    compare_main_with_expected_output(test_args, expected, capsys)
 
 
-# correct_rule = """
-# - rulename: arbitraryrulename
-#     filters:
-#         path: hello.txt
-#     actions:
-#         exists: true
-# """
 correct_rule = """
 - rulename: predictions_file
   filters:
