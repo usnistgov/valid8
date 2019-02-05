a87edbb5968fe9883ca335df9f7bded0830a4613
Marion <marion.le-bras@esial.net>
2019-02-05 10:20:58 -0500

    action and filter aliases

--- rbv/actions/__init__.py
+++ rbv/actions/__init__.py
@@ -1,6 +1,11 @@
-__all__ = ["count", "exists", "match", "scripts"]
+__all__ = ["count", "exists", "match", "scripts", "script", "find_match", "cardinality"]
 
 from .count import count
 from .exists import exists
 from .match import match
 from .scripts import scripts
+
+# Alias
+script = scripts
+find_match = match
+cardinality = count
--- rbv/filters/__init__.py
+++ rbv/filters/__init__.py
@@ -1,5 +1,4 @@
 __all__ = ["path", "path_list", "paths_from_file", "find"]
-__all__ = ["path", "path_list", "paths_from_file", "find"]
 
 from .match_paths import match_file_paths as paths_from_file
 from .match_paths import match_list_path as path_list
