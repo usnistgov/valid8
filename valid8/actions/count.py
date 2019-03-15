a19e2bfef7855382e69c7731465a0fb1b5807bf5
Peter Fontana <peter.fontana@nist.gov>
2019-03-15 15:36:08 -0400

    Added support for + in action 'count'

--- valid8/actions/count.py
+++ valid8/actions/count.py
@@ -1,6 +1,6 @@
-from ..exceptions import IncorrectFileCount
+from ..exceptions import IncorrectFileCount, InsufficientFileCount, UnknownArgument
 from ..file_utils import filepath_exists
-
+import re
 
 def count(n, context):
     """
@@ -27,7 +27,27 @@ def count(n, context):
     """
 
     existing_files = [filepath for filepath in context if filepath_exists(filepath)]
-    if len(existing_files) != n:
-        return False, [IncorrectFileCount(n, existing_files)]
+    exists_file_count = len(existing_files)
 
-    return True, []
+    count_plus_num_match = re.match("(\d+)\+",str(n))
+    # Case 1: we have n="<n>+" where <n> is an integer
+    if(count_plus_num_match is not None):
+        min_n = count_plus_num_match.group(1)
+        if(exists_file_count >= min_n):
+            return True, [],
+        else:
+            return False, [InsufficientFileCount(min_n, existing_files)]
+    # Case 2: n is an integer parseable with int(), if here not at Case 1
+    try:
+        int_n = int(n)
+        if exists_file_count != int_n:
+            return False, [IncorrectFileCount(int_n, existing_files)]
+        return True, []
+    # Case 3: improperly formatted arugment
+    except ValueError as e:
+        err_message = "count() expects and integer or an integer followed by a plus, such as '11' or `3+`. " + \
+                  "count() received argument " + n
+        raise UnknownArgument(err_message, "action", "count")
+
+
+
